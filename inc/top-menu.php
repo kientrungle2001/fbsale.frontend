c12c85197e33bb631197730661bfe485b43c28a9
Le Trung Kien <kientrungle2001@gmail.com>
2018-06-25 09:32:27 +0700

    Commit All

---
+++ inc/top-menu.php
@@ -0,0 +1,9 @@
+<!-- Navbar -->
+  <nav class="main-header navbar navbar-expand bg-white navbar-light border-bottom">
+    
+	<?php require 'top-menu/menu.php';?>
+	<?php // require 'top-menu/search.php';?>
+	<?php //require 'top-menu/right-menu.php';?>
+    
+  </nav>
+  <!-- /.navbar -->
