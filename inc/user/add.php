da4861aac98a193e59edddc1e10d19b2ca817177
huunv90 <nguyenhuu140490@gmail.com>
2018-07-10 11:44:34 +0700

    huunv

--- inc/user/add.php
+++ inc/user/add.php
@@ -6,44 +6,44 @@
   </div>
   <!-- /.card-header -->
   <!-- form start -->
-  <form role="form">
+  <form id="addForm" role="form">
 	<div class="card-body">
 	  <div class="row">
 		  <div class="form-group col-6">
 			<label for="exampleInputEmail1">Họ và Tên</label>
-			<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Họ và tên">
+			<input name="name" type="text" class="form-control" placeholder="Họ và tên">
 		  </div>
 		  
 		  <div class="form-group col-6">
 			<label for="exampleInputEmail1">Tên đăng nhập</label>
-			<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Tên đăng nhập">
+			<input name="username" type="text" class="form-control" placeholder="Tên đăng nhập">
 		  </div>
 	  </div>
 	  <div class="row">
 		  <div class="form-group col-6">
 			<label for="exampleInputPassword1">Mật khẩu</label>
-			<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Mật khẩu">
+			<input name="password" type="password" class="form-control" id="exampleInputPassword1" placeholder="Mật khẩu">
 		  </div>
 		  
 		  <div class="form-group col-6">
 			<label for="exampleInputPassword1">Xác nhận mật khẩu</label>
-			<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Mật khẩu">
+			<input name="repassword" type="password" class="form-control" id="exampleInputPassword1" placeholder="Mật khẩu">
 		  </div>
 	  </div>
 	<div class="row">
 	  <div class="form-group col-4">
 		<label for="exampleInputEmail1">Email</label>
-		<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+		<input name="email" type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
 	  </div>
 	  
 	  <div class="form-group col-4">
 		<label for="exampleInputEmail1">Số điện thoại</label>
-		<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Số điện thoại">
+		<input name="phone" type="text" class="form-control" placeholder="Số điện thoại">
 	  </div>
 	  
 	  <div class="form-group col-4">
 		<label>Giới tính</label>
-		<select class="form-control">
+		<select name="gender" class="form-control">
 		  <option value="0">Chưa xác định</option>
 		  <option value="1">Nam</option>
 		  <option value="-1">Nữ</option>
@@ -52,7 +52,7 @@
 	</div>
 	  
 	  <div class="form-check">
-		<input type="checkbox" class="form-check-input" id="status">
+		<input name="status" value="1" type="checkbox" class="form-check-input" id="status">
 		<label class="form-check-label" for="status">Hoạt động</label>
 	  </div>
 	</div>
@@ -66,3 +66,21 @@
 </div>
 <!-- /.card -->
 </div>
+<script type="text/javascript">
+	$("#addForm").submit(function(e) {
+
+    var url = "http://fbsale.vn:1337/coreusers/"; // the script where you handle the form input.
+
+    $.ajax({
+           type: "POST",
+           url: url,
+           data: $("#addForm").serialize(), // serializes the form's elements.
+           success: function(data)
+           {
+               window.location.reload();
+           }
+         });
+
+    e.preventDefault(); // avoid to execute the actual submit of the form.
+});
+</script>
