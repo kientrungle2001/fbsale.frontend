9a97e2193e5f934bc36210d316facdf2e5d2e39e
Le Trung Kien <kientrungle2001@gmail.com>
2018-07-23 08:49:39 +0700

    all

--- inc/post/list.php
+++ inc/post/list.php
@@ -9,14 +9,14 @@
 				<div class="border-bottom h45">
 					<form class="form-inline w100p"> 
 					<div style="margin-top: 1px;" class="input-group w100p"> 
-						<input name="name" class="form-control" type="search" placeholder="Tìm kiếm..." aria-label="Search" ng-model="keyword">
+						<input name="name" class="form-control" type="search" placeholder="Tìm kiếm..." aria-label="Search" ng-model="filter.keyword">
 						<div class="input-group-append"> 
 							<button class="btn btn-outline-success my-2 my-sm-0" type="submit"><i class="fa fa-search"></i></button> 
 						</div> 
 					</div> 
 					</form>
 				</div>
-				<p class="text-center" ng-show="keyword">Từ khóa: {{keyword}}</p>
+				<p class="text-center" ng-show="keyword">Từ khóa: {{filter.keyword}}</p>
 				<div class="box-content">
 					<div ng-repeat="comment in comments" class="activity-item border-bottom" ng-class="{'unread' : comment.read == false,'active': selectedComment.id == comment.id}" ng-click="selectComment(comment)">
 						<div class="left-item">
